b732cb4349a676ca7885bee37500cd01256208a0
Giaohoclaptrinh <tongiao1999vt@gmail.com>
2025-04-11 16:13:23 +0700

    test

--- src/pages/HomeWrap.jsx
+++ src/pages/HomeWrap.jsx
@@ -17,7 +17,7 @@ export default function HomeWrap({children}) {
                        userName: user.displayName,
                        email: user.email,
                    });
-                   console.log(userCurrent.email);
+                //    console.log(userCurrent.email);
                } else {
                    console.log("User not logged in");
                }
@@ -33,33 +33,29 @@ export default function HomeWrap({children}) {
 // import { auth } from '@/firebase/db.config';
 // import { onAuthStateChanged } from 'firebase/auth';
 // import React, { useEffect, useState } from 'react';
-// import { useNavigate } from 'react-router-dom';
 
 // export default function HomeWrap({ children }) {
-//     const [userCurrent, setUserCurrent] = useState(null);
+//   const [userCurrent, setUserCurrent] = useState(null);
 
-//     useEffect(() => {
-//         const unsubscribe = onAuthStateChanged(auth, (user) => {
-//             if (user) {
-//                 setUserCurrent({
-//                     userName: user.displayName || "No Name",
-//                     email: user.email,
-//                 });
-//             } else {
-//                 setUserCurrent(null);
-//             }
+//   useEffect(() => {
+//     const unsubscribe = onAuthStateChanged(auth, (user) => {
+//       if (user) {
+//         setUserCurrent({
+//           userName: user.displayName || "No Name",
+//           email: user.email,
 //         });
+//       } else {
+//         setUserCurrent(null);
+//       }
+//     });
 
-//         return () => unsubscribe(); // Cleanup để tránh memory leak
-//     }, []);
+//     return () => unsubscribe(); // Cleanup
+//   }, []);
 
-//     return (
-//         <div
-//             className={`fixed ${userCurrent ? 'min-w-top-bar' : 'min-w-full'} 
-//                 max-w-top-bar h-full mt-[70px] overflow-scroll p-2 min-h-16 
-//                 z-50 font-primary border-b border-gray-300 top-0 right-0 bg-white`}//bị lỗi fixed ${userCurrent ? 'min-w-top-bar' : 'min-w-full'
-//         >
-//             {children}
-//         </div>
-//     );
+//   return (
+//     <div className="p-6 w-full max-w-7xl mx-auto">
+//       {children}
+//     </div>
+//   );
 // }
+
